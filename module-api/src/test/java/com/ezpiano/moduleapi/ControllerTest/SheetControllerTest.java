4c999ede92d4d4e535caf9ad3266fd75f005c460
junanhouse <csd3000j@naver.com>
2024-04-16 14:05:28 +0900

    controller test

--- module-api/src/test/java/com/ezpiano/moduleapi/ControllerTest/SheetControllerTest.java
+++ module-api/src/test/java/com/ezpiano/moduleapi/ControllerTest/SheetControllerTest.java
@@ -1,16 +1,17 @@
 package com.ezpiano.moduleapi.ControllerTest;
 
-import com.ezpiano.Ezpiano.dto.Sheets.SheetListReq;
+import com.ezpiano.Ezpiano.dto.Sheets.CreateSheetReq;
+import com.ezpiano.Ezpiano.dto.Sheets.SheetListParam;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,11 +22,13 @@ public class SheetControllerTest {
 
     @Test
     public void sheetList() throws Exception{
-        SheetListReq sheetListReq = new SheetListReq(false, null, null, 1);
+        SheetListParam sheetListParam = SheetListParam.builder()
+                .page(1)
+                .build();
 
         mockMvc.perform(get("/sheets")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(sheetListReq)))
+                .content(new ObjectMapper().writeValueAsString(sheetListParam)))
                 .andExpect(jsonPath("$.success").value(true));
     }
 
@@ -35,5 +38,20 @@ public class SheetControllerTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.success").value(true));
 
+    }
+
+    @Test
+    public void sheetCreate() throws Exception{
+        CreateSheetReq createSheetReq = CreateSheetReq.builder()
+                .title("title")
+                .description("test")
+                .imgUrl("url.com")
+                .status(false)
+                .userId("testId")
+                .build();
+        mockMvc.perform(post("/sheets")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(createSheetReq)))
+                .andExpect(jsonPath("$.success").value(true));
     }
 }
--- module-api/src/test/java/com/ezpiano/moduleapi/ControllerTest/UserControllerTest.java
+++ module-api/src/test/java/com/ezpiano/moduleapi/ControllerTest/UserControllerTest.java
@@ -1,38 +1,18 @@
 package com.ezpiano.moduleapi.ControllerTest;
 
-import com.ezpiano.Ezpiano.dto.Users.LoginDefaultRes;
 import com.ezpiano.Ezpiano.dto.Users.LoginReq;
 import com.ezpiano.Ezpiano.dto.Users.SignUpDefaultReq;
-import com.ezpiano.Ezpiano.dto.Users.SignUpDefaultRes;
-import com.ezpiano.Ezpiano.entity.Sheets;
-import com.ezpiano.Ezpiano.entity.User;
-import com.ezpiano.moduleapi.controller.UserController;
-import com.ezpiano.moduleapi.repository.Users.MemoryUserRepository;
-import com.ezpiano.moduleapi.repository.Users.UserRepository;
 import com.ezpiano.moduleapi.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.List;
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 import static org.mockito.Mockito.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
